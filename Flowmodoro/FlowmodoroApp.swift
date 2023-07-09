510ba3b92ce6c9b8dddda4abc0e5b7fdb173d07f
MacioSzekTV <macioszektv@gmail.com>
2023-07-09 14:42:30 +0200

    Initial Commit

---
+++ Flowmodoro/FlowmodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowmodoroApp.swift
+//  Flowmodoro
+//
+//  Created by Maciej Łabędź on 09/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowmodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlowmodoroUITests/FlowmodoroUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FlowmodoroUITestsLaunchTests.swift
+//  FlowmodoroUITests
+//
+//  Created by Maciej Łabędź on 09/07/2023.
+//
+
+import XCTest
+
+final class FlowmodoroUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
